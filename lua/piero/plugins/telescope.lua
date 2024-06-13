62826c807e21ff3d22b45399c84285ba212df2e1
pieroGenoves3 <pierogfgenovese@gmail.com>
2024-06-13 16:48:58 -0300

    alterations: changing harpoon harpoon and telescop

--- lua/piero/plugins/telescope.lua
+++ lua/piero/plugins/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>lgp', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pl', function()
+vim.keymap.set('n', '<leader>gp', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
